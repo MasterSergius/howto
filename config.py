10354c4ea98975cc5a7d2f92b0953e54fdf664c9
Serhii Buniak <master.sergius@gmail.com>
2020-10-21 21:27:15 +0300

    Add file fs.txt, change score threshold

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # commands in file should be separated with at least 10 symbols '#'
 SEPARATOR = '#' * 10
 # show search results with score equal or higher than threshold
-THRESHOLD = 0.2
+THRESHOLD = 0.1
